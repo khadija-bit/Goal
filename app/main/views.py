0726f615074711b59305304b517a5d0e869cb770
khadija-bit <sumeyahassan34@gmail.com>
2020-04-28 15:50:40 +0300

    Added blueprint views

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,redirect,url_for
+# from flask import render_template,request,redirect,url_for
 from . import main
 from ..requests import get_sources,get_article,get_articles
 
